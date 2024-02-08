6ac61a9c97b5865baba56a5cab0ad854495ed2f7
Abhisek Datta <abhi.datta07@gmail.com>
2024-02-08 22:17:13 +0530

    Streamlit deployment

--- app.py
+++ app.py
@@ -1,20 +1,74 @@
 from fastai.vision.all import *
 import streamlit as st
+from PIL import Image
+from io import BytesIO  # Import BytesIO class from io module
 
+# Title of the Web Application
 st.title(":orange[Chest X-ray Analyzer]")
 
+# File uploader widget
 uploaded_files = st.file_uploader(
-    "Upload a Chest Xray Scan Photos", accept_multiple_files=True)
+    "Upload a Chest X-ray Scan Photo", accept_multiple_files=True)
 
-
+# Load the trained model
 learn = load_learner('image_classification_model_vgg16.pkl')
 
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    pedict = learn.predict(bytes_data)
-    st.write(pedict)
+# Function to process uploaded files and make predictions
 
 
-# pedict = learn.predict("val/NORMAL/NORMAL2-IM-1442-0001.jpeg")
+def process_files(uploaded_files):
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        img = Image.open(BytesIO(bytes_data))
 
-# print(pedict)
+        # Resize the image to a smaller size
+        # img = img.resize((100, 100))
+
+        st.image(img, caption='Uploaded Image', use_column_width=True)
+
+        with st.spinner(f'Analyzing {uploaded_file.name}...'):
+            prediction = learn.predict(bytes_data)
+            st.write(prediction)
+
+
+# Adding a submit button to trigger predictions
+if st.button('Submit'):
+    if uploaded_files:
+        process_files(uploaded_files)
+    else:
+        st.warning("Please upload an image before submitting.")
+
+
+# from fastai.vision.all import *
+# import streamlit as st
+
+# # Title of the Web Application
+# st.title(":orange[Chest X-ray Analyzer]")
+
+# st.markdown("---")
+
+# # File uploader widget
+# uploaded_files = st.file_uploader(
+#     "Upload a Chest X-ray Scan Photo", accept_multiple_files=True)
+
+
+# # Load the trained model
+# learn = load_learner('image_classification_model_vgg16.pkl')
+
+# # Function to process uploaded files and make predictions
+
+
+# def process_files(uploaded_files):
+#     for uploaded_file in uploaded_files:
+#         bytes_data = uploaded_file.read()
+#         with st.spinner(f'Analyzing {uploaded_file.name}...'):
+#             prediction = learn.predict(bytes_data)
+#             st.write(prediction)
+
+
+# # Adding a submit button to trigger predictions
+# if st.button('Submit'):
+#     if uploaded_files:
+#         process_files(uploaded_files)
+#     else:
+#         st.warning("Please upload an image before submitting.")

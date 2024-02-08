9c1f42abe01674f91c03f8300b4adfad31c9645c
Abhisek Datta <abhi.datta07@gmail.com>
2024-02-08 22:23:34 +0530

    Streamlit deployment

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from io import BytesIO  # Import BytesIO class from io module
 # Title of the Web Application
 st.title(":orange[Chest X-ray Analyzer]")
 
+st.markdown("---")
+
 # File uploader widget
 uploaded_files = st.file_uploader(
     "Upload a Chest X-ray Scan Photo", accept_multiple_files=True)
@@ -28,7 +30,7 @@ def process_files(uploaded_files):
 
         with st.spinner(f'Analyzing {uploaded_file.name}...'):
             prediction = learn.predict(bytes_data)
-            st.write(prediction)
+            st.write("The Result is : ", prediction[0])
 
 
 # Adding a submit button to trigger predictions
@@ -37,38 +39,3 @@ if st.button('Submit'):
         process_files(uploaded_files)
     else:
         st.warning("Please upload an image before submitting.")
-
-
-# from fastai.vision.all import *
-# import streamlit as st
-
-# # Title of the Web Application
-# st.title(":orange[Chest X-ray Analyzer]")
-
-# st.markdown("---")
-
-# # File uploader widget
-# uploaded_files = st.file_uploader(
-#     "Upload a Chest X-ray Scan Photo", accept_multiple_files=True)
-
-
-# # Load the trained model
-# learn = load_learner('image_classification_model_vgg16.pkl')
-
-# # Function to process uploaded files and make predictions
-
-
-# def process_files(uploaded_files):
-#     for uploaded_file in uploaded_files:
-#         bytes_data = uploaded_file.read()
-#         with st.spinner(f'Analyzing {uploaded_file.name}...'):
-#             prediction = learn.predict(bytes_data)
-#             st.write(prediction)
-
-
-# # Adding a submit button to trigger predictions
-# if st.button('Submit'):
-#     if uploaded_files:
-#         process_files(uploaded_files)
-#     else:
-#         st.warning("Please upload an image before submitting.")
